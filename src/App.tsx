389dfedd7e8cb91a983908b04d6b45600f8117aa
AlexJHC <85096994+AlexJHC@users.noreply.github.com>
2022-10-03 20:56:18 +0300

    remove change

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Cloudfront test</p>
       </header>
     </div>
   );
